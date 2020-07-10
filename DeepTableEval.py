ba74926b8e880f59d48c562310c93573bb221770
Marhabibi <m1habiby@gmail.com>
2020-07-10 12:47:19 +0200

    Add files via upload

---
+++ DeepTableEval.py
@@ -0,0 +1,51 @@
+from inputvars import *
+md1,output,inp = inputvarsEval(sys.argv[1:],sys.argv[0])
+import pandas as pd
+import numpy as np
+import random 
+import pickle
+import os
+os.environ['KERAS_BACKEND']='tensorflow'
+import keras
+from keras.layers import Embedding,Dense, Input, Flatten,Conv1D,Conv2D, MaxPooling1D, Embedding, Concatenate, Dropout,AveragePooling1D,LSTM, GRU, Bidirectional, TimeDistributed,Convolution2D,MaxPooling2D,AveragePooling2D,Permute
+from keras.layers.core import Permute
+from keras.models import Model,Sequential,load_model 
+from sklearn.metrics import classification_report as cr
+from sklearn.metrics import confusion_matrix as cm
+from keras import backend as K
+np.random.seed(813306)	
+from input_transformation import *
+
+def read_input(inp):
+	with open(inp,'rb') as f:
+		[data,Y,dicTab]=pickle.load(f)
+	train_size = int(Y.shape[0]*0.75)
+	return data[train_size:,:,:,:],Y[train_size:]
+
+	
+if __name__ == "__main__":
+	
+	# variable initialization
+	MAX_COL=9
+	MAX_COL_LENGTH=9
+	MAX_CELL_LENGTH=4
+	X_test,y_test,dictionary = transform_tables(inp, "test")
+	
+	# load model
+	model = load_model(md1)
+	
+	# predict labels
+	pred = model.predict(X_test, verbose=1)
+
+	
+	refs = [r.tolist().index(max(r.tolist())) for r in y_test]
+	preds=[p.tolist().index(max(p.tolist())) for p in pred]
+	
+	# write predictions in a file
+	refs_preds = pd.DataFrame([(r.tolist().index(max(r.tolist())),p.tolist().index(max(p.tolist()))) for r,p in zip(y_test,pred)], columns = ["reference","prediction"])
+	refs_preds.to_csv(output+".csv",index=False)
+	
+	# display performances
+	print(cr(refs,preds,digits = 4))
+	print("confusion_matrix:\n",cm(refs,preds))	
+	print("predictions are saved in \""+output+".csv\"")
---
+++ input_transformation.py
@@ -0,0 +1,65 @@
+import numpy as np
+import random 
+import pickle
+import os
+os.environ['KERAS_BACKEND']='tensorflow'
+import keras
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+
+
+
+
+
+def transform_tables(inp, config):
+
+	MAX_COL=9
+	MAX_COL_LENGTH=9
+	MAX_CELL_LENGTH=4
+	
+
+	with open(inp,'rb') as f: 
+		[X,y] = pickle.load(f)
+	
+	texts = ["XXX"] + [' '.join(text_to_word_sequence(' '.join(sum(x,[])),lower=False)) for x in X]
+	
+	
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(texts)
+	
+	
+
+	if config == "train":
+	
+		data = np.zeros((int(len(X)*0.75), MAX_COL, MAX_COL_LENGTH,MAX_CELL_LENGTH), dtype='int32')
+
+		X = X[0:int(len(X)*0.75)]
+		y = y[0:int(len(y)*0.75)]
+
+	else:
+		data = np.zeros((len(X)-int(len(X)*0.75), MAX_COL, MAX_COL_LENGTH,MAX_CELL_LENGTH), dtype='int32')
+	
+		X = X[int(len(X)*0.75):]
+		y = y[int(len(y)*0.75):]
+		
+	for i, table in enumerate(X):
+		
+		for j, col in enumerate(table):
+			
+			if j< MAX_COL:
+				
+				for k, cell in enumerate(col):
+					
+					if k<MAX_COL_LENGTH:
+						
+						z=0
+						
+						for _,word in enumerate(text_to_word_sequence(cell,lower=False)):
+							
+							if z<MAX_CELL_LENGTH:
+								
+								if tokenizer.word_index.get(word) is not None:
+									data[i,j,k,z] = tokenizer.word_index[word]
+									z=z+1                    
+	
+	return data, np.array(y),tokenizer.word_index
+	
