fd79ed5623dce38f2a796f86853930f912f620ce
Marhabibi <m1habiby@gmail.com>
2020-09-21 18:33:56 +0200

    Update input_transformation.py

--- input_transformation.py
+++ input_transformation.py
@@ -42,21 +42,13 @@ def transform_tables(inp, config):
 		y = y[int(len(y)*0.75):]
 		
 	for i, table in enumerate(X):
-		
 		for j, col in enumerate(table):
-			
 			if j< MAX_COL:
-				
 				for k, cell in enumerate(col):
-					
 					if k<MAX_COL_LENGTH:
-						
 						z=0
-						
-						for _,word in enumerate(text_to_word_sequence(cell,lower=True)):
-							
+						for _,word in enumerate(text_to_word_sequence(cell,lower=True)):	
 							if z<MAX_CELL_LENGTH:
-								
 								if tokenizer.word_index.get(word) is not None:
 									data[i,j,k,z] = tokenizer.word_index[word]
 									z=z+1                    
